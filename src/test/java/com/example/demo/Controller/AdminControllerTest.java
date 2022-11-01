2c766667ffeddb34f115f94a686b0dae8b9f4729
MarcusSit <sitmarcus@gmail.com>
2022-11-01 17:10:53 +0800

    Added Unit tests

--- src/test/java/com/example/demo/Controller/AdminControllerTest.java
+++ src/test/java/com/example/demo/Controller/AdminControllerTest.java
@@ -1,12 +1,40 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Exception.*;
+import com.example.demo.Service.Impl.AdminServiceImpl;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@RunWith(SpringJUnit4ClassRunner.class)
 class AdminControllerTest {
 
+    @Mock
+    private AdminServiceImpl adminService;
+
+    @Mock
+    private Scanner scanner = new Scanner(System.in);
+
+    @InjectMocks
+    private AdminController adminController;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
-    void flowForAdmin() {
+    void flowForAdmin() throws BookingExistsException, SeatAlreadyBookedException, ShowDoesNotExistException, TicketNotFoundException, CancellationWindowExpiredException {
+        Mockito.when(scanner.nextLine()).thenReturn("setup 1 3 3 3");
+        adminController.flowForAdmin();
     }
 }
